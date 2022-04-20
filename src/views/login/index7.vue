e0d706d9df981d21c057a8e20701a3269b87f4c9
jiawei <2056556829@qq.com>
2022-04-20 12:37:13 +0800

    66-70

---
+++ src/views/login/index7.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="m-view-masterMenu m-window">
+      <div class="m-module">
+        <el-collapse v-model="activeName" accordion :loading="loading">
+          <el-collapse-item
+            v-for="(item, index) in activeMenu"
+            :key="item.id"
+            :title="item.name"
+            :name="index"
+          >
+            <ul>
+              <li
+                v-for="child in item.children"
+                :key="child.id"
+                @click="addTab(child)"
+              >
+                <i :class="'iconfont icon-' + child.code"></i>
+                <span>{{ child.name }}</span>
+              </li>
+            </ul>
+          </el-collapse-item>
+        </el-collapse>
+      </div>
+    </div>
+  </template>
+  <script>
+  import authorize from "@/public/authorize";
+  /**
+   * @group gridExample
+   * 表格示例 入口页面
+   */
+  export default {
+    name: "gridExample",
+    data() {
+      return {
+        // 激活的菜单列表
+        activeMenu: [],
+        activeName: 0,
+        loading: false,
+      };
+    },
+    props: {
+      addTab: {
+        type: Function,
+      },
+    },
+    beforeMount() {
+      this.getAuth();
+    },
+    methods: {
+      /**
+       * @vuese
+       * 获取权限，使用路由传入的 parentId
+       */
+      getAuth() {
+        let parentId = this.$route.params.pid;
+        this.loading = true;
+        authorize.getMenuAuth(parentId).then((res) => {
+          this.activeMenu = res;
+          this.loading = false;
+        });
+      },
+      winCtrl(type) {
+      console.log(type);
+      switch (type) {
+        case "min":
+          sendMain("minWin");
+          break;
+        case "max":
+          sendMain("maxWin");
+          break;
+        case "close":
+          this.$confirm("是否确定退出程序？", "消息提示", {
+            distinguishCancelAndClose: true,
+            showClose: false,
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            customClass: "confirmAsk",
+          })
+            .then(() => {
+              this.$http
+                .userLogout({
+                  uid: this.$constant.user.id,
+                })
+                .then(() => {
+                  sendMain("closeWin", "lis");
+                })
+                .catch(() => {
+                  sendMain("closeWin", "lis");
+                });
+            })
+            .catch(() => {});
+          break;
+      }
+    },
+    },
+  };
+  </script>
+  
